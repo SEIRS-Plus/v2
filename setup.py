74de320cb34222980e3c7e2b661ce29e9887f129
James Hibbard <8717652+JHibbard@users.noreply.github.com>
2021-11-09 08:22:23 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,47 @@
+# Standard Libraries
+from setuptools import setup, find_namespace_packages
+import pathlib
+
+
+APP_NAME = "seirsplus"
+VERSION = "2.0.0"
+LICENSE = "MIT"
+AUTHOR = "Bergstrom Lab"
+DESCRIPTION = "Toolkit for modeling epidemic dynamics"
+URL = "https://github.com/SEIRS-Plus/v2.git"
+
+
+# Directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# Text of README file
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name=APP_NAME,
+    version=VERSION,
+    license=LICENSE,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=URL,
+    install_requires=[],
+    extra_require={
+        "docs": ["Sphinx>=4.2.0"],
+        "notebooks": ["jupyterlab>=3.2.0", "altair>=4.1.0"],
+        "dev": ["pre-commit>=2.15.0"],
+    },
+    packages=find_namespace_packages("src"),
+    package_dir={"": "src"},
+    package_data={"": ["*.yaml"]},
+    entry_points="""
+    [console_scripts]
+    """,
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
