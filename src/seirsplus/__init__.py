8e12b69fe2618bd01e03c8347fefdccdefc5b221
Ryan Seamus McGee <ryansmcgee@gmail.com>
2021-12-21 14:55:59 -0600

    Fixing importlib imports to support python < v 3.7

--- src/seirsplus/__init__.py
+++ src/seirsplus/__init__.py
@@ -1,11 +1,15 @@
 # Standard Libraries
-# from importlib import metadata
+try:
+	from importlib import metadata
+except ImportError:
+	# Try backported to PY<37 `importlib_metadata`.
+	import importlib_metadata as metadata
 
 # Internal Libraries
 from .dev_tools.logging_utils import _configure_seirsplus_loggers
 
 
 _configure_seirsplus_loggers(root_module_name=__name__)
-# __version__ = metadata.version(__package__)
+__version__ = metadata.version(__package__)
 
 __all__ = []
